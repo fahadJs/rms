794ef7ce884e1de6597457a82f4c882008c46746
Fahad Javed <fahad.js@yahoo.com>
2023-12-15 23:08:46 +0500

    update

--- api/items/intemController.js
+++ api/items/intemController.js
@@ -2,12 +2,12 @@ const poolConnection = require("../../config/database");
 
 const getAll = async (req, res) => {
     try {
-        let sql = 'SELECT * FROM menuitems JOIN categories ON menuitems.CategoryID = categories.CategoryID';
+        let sql = 'SELECT *, menuitems.Description as des FROM menuitems JOIN categories ON menuitems.CategoryID = categories.CategoryID';
         const result = await poolConnection.query(sql);
         const menuData = result.map(results => ({
             id: results.MenuItemID,
             name: results.Name,
-            description: results.Description,
+            description: results.des,
             price: results.Price,
             category: results.CategoryName
         }))
@@ -16,7 +16,7 @@ const getAll = async (req, res) => {
         console.error(`Error executing query! Error: ${error}`);
         res.status(500).json('Error while Fetching items!');
     }
-}
+};
 
 const create = async (req, res) => {
     try {
@@ -28,9 +28,74 @@ const create = async (req, res) => {
         console.error(`Error executing query! Error: ${error}`);
         res.status(500).json('Error while adding items!');
     }
-}
+};
+
+const update = async (req, res) => {
+    try {
+        const itemId = req.params.id;
+        const { name, description, price, categoryId } = req.body;
+
+        let sql = 'UPDATE menuitems SET Name=?, Description=?, Price=?, CategoryID=? WHERE MenuItemID=?';
+        const result = await poolConnection.query(sql, [name, description, price, categoryId, itemId]);
+
+        if (result.affectedRows > 0) {
+            res.status(200).json('Record Updated Successfully!');
+        } else {
+            res.status(404).json('Record not found or no changes made.');
+        }
+    } catch (error) {
+        console.error(`Error executing query! Error: ${error}`);
+        res.status(500).json('Error while updating item!');
+    }
+};
+
+const getById = async (req, res) => {
+    try {
+        const itemId = req.params.id;
+        let sql = 'SELECT * FROM menuitems WHERE MenuItemID = ?';
+        const result = await poolConnection.query(sql, [itemId]);
+
+        if (result.length > 0) {
+            const itemData = {
+                id: result[0].MenuItemID,
+                name: result[0].Name,
+                description: result[0].Description,
+                price: result[0].Price,
+                category: result[0].CategoryID,
+            };
+            res.status(200).json(itemData);
+        } else {
+            res.status(404).json('Item not found.');
+        }
+    } catch (error) {
+        console.error(`Error executing query! Error: ${error}`);
+        res.status(500).json('Error while fetching item!');
+    }
+};
+
+const deleteItem = async (req, res) => {
+    try {
+        const itemId = req.params.id;
+        let sql = 'DELETE FROM menuitems WHERE MenuItemID = ?';
+        const result = await poolConnection.query(sql, [itemId]);
+
+        if (result.affectedRows > 0) {
+            res.status(200).json('Record Deleted Successfully!');
+        } else {
+            res.status(404).json('Item not found or already deleted.');
+        }
+    } catch (error) {
+        console.error(`Error executing query! Error: ${error}`);
+        res.status(500).json('Error while deleting item!');
+    }
+};
+
+
 
 module.exports = {
     getAll,
-    create
+    create,
+    update,
+    getById,
+    deleteItem
 }
--- api/items/itemRouter.js
+++ api/items/itemRouter.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const itemController = require('../items/intemController');
 
 router.get('/', itemController.getAll);
+router.get('/:id', itemController.getById);
 router.post('/', itemController.create);
+router.patch('/:id', itemController.update);
 
 module.exports = router;
