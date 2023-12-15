971c1b28b118665f6cc985e7c4570d6962d6fe22
Fahad Javed <fahad.js@yahoo.com>
2023-12-15 19:27:30 +0500

    updated CAIK api

--- api/category-and-items-kitchen/caikController.js
+++ api/category-and-items-kitchen/caikController.js
@@ -3,17 +3,19 @@ const poolConnection = require('../../config/database');
 const getAll = async (req, res) => {
     try {
         let sql = 'SELECT categories.CategoryID, categories.CategoryName, menuitems.MenuItemID, menuitems.Name as itemName, menuitems.Description, menuitems.Price, inventory.Quantity, kitchens.Name FROM categories LEFT JOIN menuitems ON categories.CategoryID = menuitems.CategoryID LEFT JOIN menuitem_categories ON menuitems.MenuItemID = menuitem_categories.MenuItemID LEFT JOIN inventory ON menuitems.MenuItemID = inventory.MenuItemID LEFT JOIN kitchens ON menuitem_categories.KitchenID = kitchens.KitchenID';
+        
         const result = await poolConnection.query(sql);
 
         const caikData = result.reduce((acc, row) => {
-            if (!acc[row.CategoryID]) {
-                acc[row.CategoryID] = {
+            if (!acc[row.CategoryName]) {
+                acc[row.CategoryName] = {
+                    category_id: row.CategoryID,
                     category_name: row.CategoryName,
                     items: [],
                 };
             }
             if (row.MenuItemID) {
-                acc[row.CategoryID].items.push({
+                acc[row.CategoryName].items.push({
                     item_name: row.itemName,
                     description: row.Description,
                     price: row.Price,
