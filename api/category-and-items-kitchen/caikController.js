ea076ae12f52c60ce66304220da29e237ead3f9c
Fahad Javed <fahad.js@yahoo.com>
2023-12-17 16:32:23 +0500

    updated furthur!

--- api/category-and-items-kitchen/caikController.js
+++ api/category-and-items-kitchen/caikController.js
@@ -1,8 +1,48 @@
 const poolConnection = require('../../config/database');
 
+// const getAll = async (req, res) => {
+//     try {
+//         let sql = 'SELECT menuitems.MenuItemID, menuitems.Name, menuitems.Description, menuitems.Price, categories.CategoryName, categories.CategoryID, inventory.Quantity, kitchens.Name AS kitchenName FROM menuitems JOIN categories ON menuitems.CategoryID = categories.CategoryID LEFT JOIN menuitem_categories ON menuitems.MenuItemID = menuitem_categories.MenuItemID LEFT JOIN inventory ON menuitems.MenuItemID = inventory.MenuItemID LEFT JOIN kitchens ON menuitem_categories.KitchenID = kitchens.KitchenID';
+        
+//         const result = await poolConnection.query(sql);
+
+//         const menuData = result.map(results => ({
+//             item_id: results.MenuItemID,
+//             item_name: results.Name,
+//             item_description: results.Description,
+//             item_price: results.Price,
+//             category_id: results.CategoryID,
+//             category_name: results.CategoryName,
+//             item_quantity: results.Quantity,
+//             kitchen_name: results.kitchenName
+//         }))
+//         res.status(200).json(menuData);
+//     } catch (error) {
+//         console.error(`Error executing query! Error: ${error}`);
+//         res.status(500).json('Error while Fetching items!');
+//     }
+// }
+
 const getAll = async (req, res) => {
     try {
-        let sql = 'SELECT menuitems.MenuItemID, menuitems.Name, menuitems.Description, menuitems.Price, categories.CategoryName, categories.CategoryID, inventory.Quantity, kitchens.Name AS kitchenName FROM menuitems JOIN categories ON menuitems.CategoryID = categories.CategoryID LEFT JOIN menuitem_categories ON menuitems.MenuItemID = menuitem_categories.MenuItemID LEFT JOIN inventory ON menuitems.MenuItemID = inventory.MenuItemID LEFT JOIN kitchens ON menuitem_categories.KitchenID = kitchens.KitchenID';
+        let sql = `
+            SELECT 
+                menuitems.MenuItemID,
+                menuitems.Name,
+                menuitems.Description,
+                menuitems.Price,
+                categories.CategoryName,
+                categories.CategoryID,
+                inventory.Quantity,
+                kitchens.Name AS kitchenName,
+                kitchens.KitchenID AS kitchenid
+            FROM 
+                menuitems
+                JOIN menuitem_categories ON menuitems.MenuItemID = menuitem_categories.MenuItemID
+                JOIN categories ON menuitem_categories.CategoryID = categories.CategoryID
+                LEFT JOIN inventory ON menuitems.MenuItemID = inventory.MenuItemID
+                LEFT JOIN kitchens ON menuitem_categories.KitchenID = kitchens.KitchenID
+        `;
         
         const result = await poolConnection.query(sql);
 
@@ -14,12 +54,14 @@ const getAll = async (req, res) => {
             category_id: results.CategoryID,
             category_name: results.CategoryName,
             item_quantity: results.Quantity,
-            kitchen_name: results.kitchenName
-        }))
+            kitchen_name: results.kitchenName,
+            kitchen_id: results.kitchenid
+        }));
+
         res.status(200).json(menuData);
     } catch (error) {
         console.error(`Error executing query! Error: ${error}`);
-        res.status(500).json('Error while Fetching items!');
+        res.status(500).json('Error while fetching items!');
     }
 }
 
