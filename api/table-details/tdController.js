5a5add6377036fca85670a6e23f542d1ed5d6dd6
Fahad Javed <fahad.js@yahoo.com>
2023-12-18 18:47:51 +0500

    added PAID CANCEL

--- api/table-details/tdController.js
+++ api/table-details/tdController.js
@@ -37,7 +37,7 @@ const getAll = async (req, res) => {
         const result = await poolConnection.query(query, [tableId]);
 
         if (result.length == 0) {
-            res.status(200).json({message: "Table is already PAID and AVAILABLE!"});
+            res.status(200).json({ message: "Table is already PAID and AVAILABLE!" });
         } else {
             const formattedResult = result.map(order => ({
                 ...order,
@@ -51,8 +51,64 @@ const getAll = async (req, res) => {
         console.error(`Error executing query! Error: ${error}`);
         res.status(500).json('Error while fetching order details!');
     }
+};
+
+const mrkPaid = async (req, res) => {
+    try {
+        const orderId = req.params.orderId;
+
+        const updateOrderQuery = 'UPDATE orders SET order_status = "paid" WHERE OrderID = ?';
+        await poolConnection.query(updateOrderQuery, [orderId]);
+
+        const updateTableQuery = 'UPDATE tables SET status = "available" WHERE table_id = (SELECT table_id FROM orders WHERE OrderID = ?)';
+        await poolConnection.query(updateTableQuery, [orderId]);
+
+        res.status(200).json({ message: 'Order status updated to "paid" and table status set to "available" successfully!' });
+    } catch (error) {
+        console.error(`Error executing query! Error: ${error}`);
+        res.status(500).json('Error updating order status and table status!');
+    }
+}
+
+const cancel = async (req, res) => {
+    try {
+        const orderId = req.params.orderId;
+
+        await poolConnection.query('START TRANSACTION');
+
+        const getTableIdQuery = 'SELECT table_id FROM orders WHERE OrderID = ?';
+        const tableIdResult = await poolConnection.query(getTableIdQuery, [orderId]);
+
+        if (tableIdResult.length === 0) {
+            await poolConnection.query('ROLLBACK');
+            return res.status(404).json({ message: 'Order not found!' });
+        }
+
+        const tableId = tableIdResult[0].table_id;
+
+        const deleteOrderItemsQuery = 'DELETE FROM order_items WHERE OrderID = ?';
+        await poolConnection.query(deleteOrderItemsQuery, [orderId]);
+
+        const deleteOrderQuery = 'DELETE FROM orders WHERE OrderID = ?';
+        await poolConnection.query(deleteOrderQuery, [orderId]);
+
+        const updateTableQuery = 'UPDATE tables SET status = "available" WHERE table_id = ?';
+        await poolConnection.query(updateTableQuery, [tableId]);
+
+        await poolConnection.query('COMMIT');
+
+        res.status(200).json({ message: 'Order deleted successfully and table status set to "available"!' });
+        
+    } catch (error) {
+        await poolConnection.query('ROLLBACK');
+
+        console.error(`Error executing query! Error: ${error}`);
+        res.status(500).json('Error deleting order and updating table status!');
+    }
 }
 
 module.exports = {
-    getAll
+    getAll,
+    mrkPaid,
+    cancel
 }
--- api/table-details/tdRouter.js
+++ api/table-details/tdRouter.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const tdController = require('./tdController');
 
 router.get('/:id', tdController.getAll);
+router.patch('/:id/paid', tdController.mrkPaid);
+router.delete('/', tdController.cancel);
 
 module.exports = router;
