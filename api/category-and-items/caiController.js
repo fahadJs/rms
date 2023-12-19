93a457692b9aeb134e667ca626c1585be35b08af
Fahad Javed <fahad.js@yahoo.com>
2023-12-19 17:38:25 +0500

    CAI v2

--- api/category-and-items/caiController.js
+++ api/category-and-items/caiController.js
@@ -26,7 +26,7 @@ const getAll = async (req, res) => {
                 LEFT JOIN menuitem_categories ON categories.CategoryID = menuitem_categories.CategoryID
                 LEFT JOIN subcategories ON menuitem_categories.SubcategoryID = subcategories.SubcategoryID
         `;
-        
+
         const result = await poolConnection.query(sql);
 
         const categoriesData = result.map(row => ({
@@ -40,7 +40,7 @@ const getAll = async (req, res) => {
                 }
             ],
         }));
-        
+
         res.status(200).json(categoriesData);
     } catch (error) {
         console.error(`Error executing query! Error: ${error}`);
@@ -48,7 +48,45 @@ const getAll = async (req, res) => {
     }
 }
 
+const getAll2 = async (req, res) => {
+    try {
+        let sql = `
+            SELECT 
+                subcategories.SubcategoryID AS subcategory_id,
+                subcategories.SubcategoryName AS subcategory_name,
+                JSON_ARRAYAGG(
+                    JSON_OBJECT(
+                        'item_id', menuitems.MenuItemID,
+                        'item_name', menuitems.Name,
+                        'item_description', menuitems.Description,
+                        'item_price', menuitems.Price
+                    )
+                ) AS items
+            FROM 
+                menuitem_categories
+                LEFT JOIN subcategories ON menuitem_categories.SubcategoryID = subcategories.SubcategoryID
+                LEFT JOIN menuitems ON menuitem_categories.MenuItemID = menuitems.MenuItemID
+            GROUP BY
+                subcategory_id, subcategory_name
+        `;
+
+        const result = await poolConnection.query(sql);
+
+        const subcategoriesData = result.map(row => ({
+            subcategory_id: row.subcategory_id,
+            subcategory_name: row.subcategory_name,
+            items: JSON.parse(row.items),
+        }));
+
+        res.status(200).json(subcategoriesData);
+    } catch (error) {
+        console.error(`Error executing query! Error: ${error}`);
+        res.status(500).json('Error while fetching subcategories and items!');
+    }
+}
+
 
 module.exports = {
-    getAll
+    getAll,
+    getAll2
 }
--- api/category-and-items/caiRouter.js
+++ api/category-and-items/caiRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const caiController = require('./caiController');
 
 router.get('/', caiController.getAll);
+router.get('/v2', caiController.getAll2);
 
 module.exports = router;
