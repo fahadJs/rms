ecc76ae0975ddbda629cb63f516037e355ffafc9
Fahad Javed <fahad.js@yahoo.com>
2023-12-19 17:44:35 +0500

    CAI v2 By ID

--- api/category-and-items/caiController.js
+++ api/category-and-items/caiController.js
@@ -85,8 +85,55 @@ const getAll2 = async (req, res) => {
     }
 }
 
+const getAll2ById = async (req, res) => {
+    try {
+        const subcategoryId = req.params.id;
+
+        let sql = `
+            SELECT 
+                subcategories.SubcategoryID AS subcategory_id,
+                subcategories.SubcategoryName AS subcategory_name,
+                JSON_ARRAYAGG(
+                    JSON_OBJECT(
+                        'item_id', menuitems.MenuItemID,
+                        'item_name', menuitems.Name,
+                        'item_description', menuitems.Description,
+                        'item_price', menuitems.Price
+                    )
+                ) AS items
+            FROM 
+                menuitem_categories
+                LEFT JOIN subcategories ON menuitem_categories.SubcategoryID = subcategories.SubcategoryID
+                LEFT JOIN menuitems ON menuitem_categories.MenuItemID = menuitems.MenuItemID
+            WHERE 
+                subcategories.SubcategoryID = ?
+            GROUP BY
+                subcategory_id, subcategory_name
+        `;
+
+        const result = await poolConnection.query(sql, [subcategoryId]);
+
+        if (result.length === 0) {
+            res.status(404).json({ message: 'Subcategory not found!' });
+            return;
+        }
+
+        const subcategoryData = result.map(row => ({
+            subcategory_id: row.subcategory_id,
+            subcategory_name: row.subcategory_name,
+            items: JSON.parse(row.items),
+        }))[0];
+
+        res.status(200).json(subcategoryData);
+    } catch (error) {
+        console.error(`Error executing query! Error: ${error}`);
+        res.status(500).json('Error while fetching subcategory and items!');
+    }
+}
+
 
 module.exports = {
     getAll,
-    getAll2
+    getAll2,
+    getAll2ById
 }
--- api/category-and-items/caiRouter.js
+++ api/category-and-items/caiRouter.js
@@ -3,5 +3,6 @@ const caiController = require('./caiController');
 
 router.get('/', caiController.getAll);
 router.get('/v2', caiController.getAll2);
+router.get('/v2/:id', caiController.getAll2ById);
 
 module.exports = router;
