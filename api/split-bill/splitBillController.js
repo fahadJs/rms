6243e724a780fa815700aa948f2805ac2aeffb10
Fahad Javed <fahad.js@yahoo.com>
2023-12-23 18:51:18 +0500

    table status and order status updated

--- api/split-bill/splitBillController.js
+++ api/split-bill/splitBillController.js
@@ -52,39 +52,39 @@ const createEqSplit = async (req, res) => {
 const createItSplit = async (req, res) => {
     try {
         await poolConnection.query('START TRANSACTION');
-    
+
         const { orderId, items } = req.body;
-    
+
         // Fetch order details
         const getOrderQuery = 'SELECT * FROM orders WHERE OrderID = ?';
         const orderResult = await poolConnection.query(getOrderQuery, [orderId]);
-    
+
         if (orderResult.length === 0) {
             throw new Error('Order not found!');
         }
-    
+
         const fetchedOrder = orderResult[0];
-    
+
         if (fetchedOrder.order_status === 'paid') {
             throw new Error('Bill is already paid!');
         }
-    
+
         // Fetch item details from order_items table
         const getItemDetailsQuery = 'SELECT * FROM order_items WHERE OrderID = ?';
         const itemDetailsResult = await poolConnection.query(getItemDetailsQuery, [orderId]);
-    
+
         // Calculate and insert split amounts
         const insertSplitItemQuery = 'INSERT INTO bill_split_item (OrderID, MenuItemID, ItemName, SplitAmount) VALUES (?, ?, ?, ?)';
         const updateOrderItemQuantityQuery = 'UPDATE order_items SET Quantity = ? WHERE OrderID = ? AND MenuItemID = ?';
         const deleteOrderItemQuery = 'DELETE FROM order_items WHERE OrderID = ? AND MenuItemID = ?';
-    
+
         for (const item of items) {
             const itemDetails = itemDetailsResult.find(details => details.MenuItemID === item.menuitemID);
-    
+
             if (itemDetails) {
                 const itemTotal = itemDetails.Price * item.quantity;
                 const itemSplitAmount = (itemTotal / fetchedOrder.total_amount) * fetchedOrder.total_amount;
-    
+
                 // Update order_items quantity
                 const updatedQuantity = itemDetails.Quantity - item.quantity;
                 if (updatedQuantity > 0) {
@@ -93,19 +93,27 @@ const createItSplit = async (req, res) => {
                     // If quantity becomes zero, delete the record
                     await poolConnection.query(deleteOrderItemQuery, [orderId, item.menuitemID]);
                 }
-    
+
                 // Insert into bill_split_item table
                 await poolConnection.query(insertSplitItemQuery, [orderId, item.menuitemID, itemDetails.ItemName, itemSplitAmount]);
             }
         }
-    
-        // const updateOrderStatusQuery = 'UPDATE orders SET order_status = "paid" WHERE OrderID = ?';
-        // await poolConnection.query(updateOrderStatusQuery, [orderId]);
-    
-        // const updateTableStatusQuery = 'UPDATE tables SET status = ? WHERE table_id = ?';
-        // const updateTableStatusValues = ['available', fetchedOrder.table_id];
-        // await poolConnection.query(updateTableStatusQuery, updateTableStatusValues);
-    
+
+        // Check if there are any remaining items in order_items
+        const remainingItemsQuery = 'SELECT COUNT(*) AS remainingItems FROM order_items WHERE OrderID = ?';
+        const remainingItemsResult = await poolConnection.query(remainingItemsQuery, [orderId]);
+        const remainingItemCount = remainingItemsResult[0].remainingItems;
+
+        if (remainingItemCount === 0) {
+            // If no remaining items, update order status to "paid" and table status to "available"
+            const updateOrderStatusQuery = 'UPDATE orders SET order_status = "paid" WHERE OrderID = ?';
+            await poolConnection.query(updateOrderStatusQuery, [orderId]);
+
+            const updateTableStatusQuery = 'UPDATE tables SET status = ? WHERE table_id = ?';
+            const updateTableStatusValues = ['available', fetchedOrder.table_id];
+            await poolConnection.query(updateTableStatusQuery, updateTableStatusValues);
+        }
+
         await poolConnection.query('COMMIT');
         res.status(201).json({ message: 'Bill split successfully!' });
     } catch (error) {
@@ -113,7 +121,8 @@ const createItSplit = async (req, res) => {
         console.error(`Error splitting bill! Error: ${error.message}`);
         res.status(500).json({ error: `Error splitting bill! ${error.message}` });
     }
-    
+
+
 
 }
 
