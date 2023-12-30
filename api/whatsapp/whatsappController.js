eb5d1249358e29430f88ce124ab9b97f32070d81
Fahad Javed <fahad.js@yahoo.com>
2023-12-30 22:13:27 +0500

    post api for whatsapp group ids

--- api/whatsapp/whatsappController.js
+++ api/whatsapp/whatsappController.js
@@ -4,17 +4,52 @@ const getAllInstances = async (req, res) => {
     try {
         // Fetch data from the database
         const rows = await poolConnection.query('SELECT * FROM WhatsAppInstances');
-    
+
         // Send the data as a JSON response
         res.status(200).json(rows);
-      } catch (error) {
+    } catch (error) {
         console.error(`Error fetching WhatsAppInstances: ${error}`);
-        res.status(500).json({ error: 'Internal Server Error' });
-      }
+        res.status(500).json({status: 500, message: 'Internal Server Error' });
+    }
 }
 
+const createGroupIds = async (req, res) => {
+    const { instance_id, w_group_number, KitchenID } = req.body;
+    const restaurant_id  = req.params;
+
+    try {
+        // Start a transaction
+        await executeQuery('START TRANSACTION');
+
+        // Insert into WGroupIds table
+        const insertGroupQuery = 'INSERT INTO WGroupIds (w_group_number) VALUES (?)';
+        const [groupInsertResult] = await executeQuery(insertGroupQuery, [w_group_number]);
+
+        // Get the generated w_group_id
+        const w_group_id = groupInsertResult.insertId;
+
+        // Insert into w_message_connection table
+        const insertConnectionQuery = `
+            INSERT INTO w_message_connection (instance_id, w_group_id, KitchenID, restaurant_id)
+            VALUES (?, ?, ?, ?)
+        `;
+        await executeQuery(insertConnectionQuery, [instance_id, w_group_id, KitchenID, restaurant_id]);
+
+        // Commit the transaction
+        await executeQuery('COMMIT');
+
+        res.status(200).json({status: 200, message: 'Data inserted successfully!' });
+    } catch (error) {
+        // Rollback the transaction in case of an error
+        await executeQuery('ROLLBACK');
+
+        console.error(`Error executing query! Error: ${error}`);
+        res.status(500).json({status: 500, message: 'Error inserting data', error: error.message });
+    }
+};
+
 const update = async (req, res) => {
-    
+
 };
 
 const create = async (req, res) => {
@@ -22,5 +57,6 @@ const create = async (req, res) => {
 };
 
 module.exports = {
-    getAllInstances
+    getAllInstances,
+    createGroupIds
 };
--- api/whatsapp/whatsappRouter.js
+++ api/whatsapp/whatsappRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const whatsappController = require('./whatsappController');
 
 router.get('/', whatsappController.getAllInstances);
+router.post('/res/:restaurant_id', whatsappController.createGroupIds);
 
 module.exports = router;
