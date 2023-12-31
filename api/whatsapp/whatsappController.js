bc6bb2141672039c08bb51f8e3185b6ef677c643
Fahad Javed <fahad.js@yahoo.com>
2023-12-31 16:03:55 +0500

    updated whatsapp code

--- api/whatsapp/whatsappController.js
+++ api/whatsapp/whatsappController.js
@@ -2,10 +2,8 @@ const poolConnection = require('../../config/database');
 
 const getAllInstances = async (req, res) => {
     try {
-        // Fetch data from the database
         const rows = await poolConnection.query('SELECT * FROM WhatsAppInstances');
 
-        // Send the data as a JSON response
         res.status(200).json(rows);
     } catch (error) {
         console.error(`Error fetching WhatsAppInstances: ${error}`);
@@ -18,29 +16,23 @@ const createGroupIds = async (req, res) => {
     const restaurant_id  = req.params;
 
     try {
-        // Start a transaction
         await executeQuery('START TRANSACTION');
 
-        // Insert into WGroupIds table
         const insertGroupQuery = 'INSERT INTO WGroupIds (w_group_number) VALUES (?)';
         const [groupInsertResult] = await executeQuery(insertGroupQuery, [w_group_number]);
 
-        // Get the generated w_group_id
         const w_group_id = groupInsertResult.insertId;
 
-        // Insert into w_message_connection table
         const insertConnectionQuery = `
             INSERT INTO w_message_connection (instance_id, w_group_id, KitchenID, restaurant_id)
             VALUES (?, ?, ?, ?)
         `;
         await executeQuery(insertConnectionQuery, [instance_id, w_group_id, KitchenID, restaurant_id]);
 
-        // Commit the transaction
         await executeQuery('COMMIT');
 
         res.status(200).json({status: 200, message: 'Data inserted successfully!' });
     } catch (error) {
-        // Rollback the transaction in case of an error
         await executeQuery('ROLLBACK');
 
         console.error(`Error executing query! Error: ${error}`);
