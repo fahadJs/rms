d6c0fca20e1293e87f4597492a38a5dd24e9e5d0
Fahad Javed <fahad.js@yahoo.com>
2024-01-03 13:26:18 +0500

    expense by month for waiter

--- api/expense/expenseController.js
+++ api/expense/expenseController.js
@@ -1,7 +1,7 @@
 const poolConnection = require('../../config/database');
 
 const getPosMonthlyExpense = async (req, res) => {
-    const {restaurantId} = req.params;
+    const { restaurantId } = req.params;
     try {
         const query = `
         SELECT 
@@ -15,32 +15,38 @@ const getPosMonthlyExpense = async (req, res) => {
       ORDER BY name;
     `;
         const data = await poolConnection.query(query, [restaurantId]);
-    res.json({data});
+        res.json({ data });
     } catch (error) {
         console.error(`Error fetching monthly report: ${error.message}`);
-        res.status(500).json({status: 500, message: 'Internal Server Error' });
+        res.status(500).json({ status: 500, message: 'Internal Server Error' });
     }
 }
 
 const getWaiterMonthlyExpense = async (req, res) => {
-    const {restaurantId} = req.params;
+    const { restaurantId } = req.params;
     try {
         const query = `
         SELECT 
-        DATE_FORMAT(o.time, '%b') AS name,
-        SUM(oi.Price * oi.Quantity) AS Expense,
-        SUM(o.total_amount) AS Income
-      FROM orders o
-      JOIN order_items oi ON o.OrderID = oi.OrderID
-      WHERE o.restaurant_id = ?
-      GROUP BY name
-      ORDER BY name;
+    DATE_FORMAT(o.time, '%b') AS month,
+    oi.ItemName,
+    SUM(oi.Quantity * mi.CostPrice) AS ItemExpense,
+    SUM(oi.Price) AS TotalIncome
+FROM 
+    orders o
+    JOIN order_items oi ON o.OrderID = oi.OrderID
+    JOIN menuitems mi ON oi.MenuItemID = mi.MenuItemID
+WHERE 
+    o.restaurant_id = ?
+GROUP BY 
+    month, oi.ItemName
+ORDER BY 
+    month, ItemExpense DESC;
     `;
         const data = await poolConnection.query(query, [restaurantId]);
-    res.json({data});
+        res.json({ data });
     } catch (error) {
         console.error(`Error fetching monthly report: ${error.message}`);
-        res.status(500).json({status: 500, message: 'Internal Server Error' });
+        res.status(500).json({ status: 500, message: 'Internal Server Error' });
     }
 }
 
