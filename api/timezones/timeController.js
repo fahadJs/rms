11f05f1965e83317f8e45d1055dc4f8ee675afc1
Fahad Javed <fahad.js@yahoo.com>
2024-01-03 12:01:09 +0500

    default currency and timezone

--- api/timezones/timeController.js
+++ api/timezones/timeController.js
@@ -12,6 +12,19 @@ const getAll = async (req, res) => {
   }
 }
 
+const getDefault = async (req, res) => {
+  const {restaurantId} = req.params;
+  try {
+    const defaultTimezonesQuery = 'SELECT * FROM restaurants WHERE restaurant_id = ?';
+    const timezone = await poolConnection.query(defaultTimezonesQuery, [restaurantId]);
+
+    res.status(200).json({timezone});
+  } catch (error) {
+    console.error(`Error fetching time zones: ${error.message}`);
+    res.status(500).json({ status: 500, message: 'Internal Server Error' });
+  }
+}
+
 
 const update = async (req, res) => {
   const {restaurantId} = req.params;
@@ -29,5 +42,6 @@ const update = async (req, res) => {
 
 module.exports = {
   getAll,
-  update
+  update,
+  getDefault
 }
--- api/timezones/timeRouter.js
+++ api/timezones/timeRouter.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const timeController = require('./timeController');
 
 router.get('/', timeController.getAll);
+router.get('/res/:restaurantId', timeController.getDefault);
 router.patch('/:restaurantId', timeController.update);
 
 module.exports = router;
