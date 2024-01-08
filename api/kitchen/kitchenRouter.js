112d730a67fd97fa35ba210179bb7da28aa3869e
Fahad Javed <fahad.js@yahoo.com>
2024-01-08 18:15:20 +0500

    semicolon

--- api/kitchen/kitchenRouter.js
+++ api/kitchen/kitchenRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const kitchenController = require('./kitchenController');
 
 router.get('/res/:restaurant_id', kitchenController.getAll);
-router.post('/res/restaurant_id', kitchenController.create);
+router.post('/res/:restaurant_id', kitchenController.create);
 router.get('/res/:restaurant_id/kit/:kitchenId', kitchenController.getById);
 router.patch('/res/:restaurant_id/kit/:kitchenId', kitchenController.update);
 router.delete('/res/:restaurant_id/kit/:kitchenId', kitchenController.remove);
