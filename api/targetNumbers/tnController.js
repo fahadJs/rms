d610a4ef6dab00c7b1057cb17c659ec9718e8c44
Fahad Javed <fahad.js@yahoo.com>
2024-01-22 22:05:02 +0500

    updated get for task software

--- api/targetNumbers/tnController.js
+++ api/targetNumbers/tnController.js
@@ -1,20 +1,19 @@
 const poolConnection = require('../../config/db');
 
-const getAll = async (req, res) => {
+const getAllCust = async (req, res) => {
     try {
-        const getAllNumbers = `SELECT * FROM target_numbers`;
+        const getAllNumbers = `SELECT * FROM cust_numbers`;
         const getAllNumbersResult = await poolConnection.query(getAllNumbers);
 
         res.status(200).json(getAllNumbersResult);
 
     } catch (error) {
-        res.status(404).json({ status: 404, message: `Error fetching Numbers!` });
+        res.status(404).json({ status: 404, message: `Error fetching Customers!` });
     }
 }
 
 const assignCustomerTask = async (req, res) => {
     try {
-
         const { cust_number } = req.body;
 
         const insertCustQuery = 'INSERT INTO cust_numbers (cust_number) VALUES (?)';
@@ -36,7 +35,34 @@ const assignCustomerTask = async (req, res) => {
     }
 }
 
+const getAllByCust = async (req, res) => {
+    try {
+        const {custId} = req.params;
+
+        const selectQuery =
+            `SELECT cn.cust_id, cn.cust_number, tn.t_num FROM cust_numbers cn LEFT JOIN target_numbers tn ON cn.cust_id = tn.cust_id WHERE tn.cust_id = ? AND tn.t_status = ?`;
+
+        const rows = await poolConnection.query(selectQuery, [custId, 'assigned']);
+
+        if (rows.length === 0) {
+            res.status(404).json({ success: false, message: 'Customer not found or no assigned numbers' });
+        } else {
+            const customerDetails = {
+                cust_id: rows[0].cust_id,
+                cust_number: rows[0].cust_number,
+                assigned_numbers: rows.map((row) => row.t_num),
+            };
+
+            res.json({customerDetails });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ status: 500, message: 'Internal Server Error' });
+    }
+}
+
 module.exports = {
-    getAll,
-    assignCustomerTask
+    getAllCust,
+    assignCustomerTask,
+    getAllByCust
 }
--- api/targetNumbers/tnRouter.js
+++ api/targetNumbers/tnRouter.js
@@ -1,7 +1,8 @@
 const tnController = require('./tnController');
 const router = require('express').Router();
 
-router.get('/get', tnController.getAll);
+router.get('/get/:custId', tnController.getAllByCust);
 router.post('/assign', tnController.assignCustomerTask);
+router.get('/get', tnController.getAllCust);
 
 module.exports = router;
