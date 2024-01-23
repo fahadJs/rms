51d1fb15163b2e47fdf107d05b7c83e98fc73bc0
Fahad Javed <fahad.js@yahoo.com>
2024-01-23 21:15:48 +0500

    whatsapp api call

--- api/targetNumbers/tnController.js
+++ api/targetNumbers/tnController.js
@@ -1,8 +1,21 @@
 const poolConnection = require('../../config/db');
+const axios = require('axios');
 
 const getAllCust = async (req, res) => {
     try {
         const getAllNumbers = `SELECT * FROM cust_numbers`;
+        const getAllNumbersResult = await poolConnection.query(getAllNumbers);
+
+        res.status(200).json(getAllNumbersResult);
+
+    } catch (error) {
+        res.status(404).json({ status: 404, message: `Error fetching Customers!` });
+    }
+}
+
+const getAllTargetNumbers = async (req, res) => {
+    try {
+        const getAllNumbers = `SELECT * FROM target_numbers`;
         const getAllNumbersResult = await poolConnection.query(getAllNumbers);
 
         res.status(200).json(getAllNumbersResult);
@@ -48,6 +61,25 @@ const getAllByCust = async (req, res) => {
             res.status(404).json({ success: false, message: 'Customer not found or no assigned numbers' });
         }
 
+        const numbersList = rows.map((row) => row.t_num).join('\n\n');
+        const message = `Paisay kamanay ka tareeqa yeh hai k:
+
+        step 1:
+        Humein "250" likh kar message karain.
+        
+        step 2:
+        Jo message aap ko receive ho, woh message diay gai neechay number per send kar dain.
+        
+        Step 3:
+        
+        Screen recording kr k 1 ghantay k baad humein bhej dena hai (bank account number or easypaisa or jazz cash)
+        
+        Inn numbers per click kar k "hi" ka message karna hai pahlay aur phir message forward karna hai picture k sath wala hai woh apko forward karna hai neechay diay gai number per.
+         
+         numbers: \n${numbersList}`;
+
+        console.log(message);
+
         res.status(200).json({
             cust_id: rows[0].cust_id,
             cust_number: rows[0].cust_number,
@@ -60,8 +92,62 @@ const getAllByCust = async (req, res) => {
     }
 }
 
+const sendMessage = async (req, res) => {
+    try {
+        const { custId } = req.params;
+
+        const selectQuery =
+            `SELECT cn.cust_id, cn.cust_number, tn.t_num FROM cust_numbers cn LEFT JOIN target_numbers tn ON cn.cust_id = tn.cust_id WHERE tn.cust_id = ? AND tn.t_status = ?`;
+
+        const rows = await poolConnection.query(selectQuery, [custId, 'assigned']);
+
+        if (rows.length === 0) {
+            res.status(404).json({ success: false, message: 'Customer not found or no assigned numbers' });
+        }
+
+        const numbersList = rows.map((row) => row.t_num).join('\n\n');
+        const message = `Paisay kamanay ka tareeqa yeh hai k:
+
+        step 1:
+        Humein "250" likh kar message karain.
+        
+        step 2:
+        Jo message aap ko receive ho, woh message diay gai neechay number per send kar dain.
+        
+        Step 3:
+        
+        Screen recording kr k 1 ghantay k baad humein bhej dena hai (bank account number or easypaisa or jazz cash)
+        
+        Inn numbers per click kar k "hi" ka message karna hai pahlay aur phir message forward karna hai picture k sath wala hai woh apko forward karna hai neechay diay gai number per.
+         
+         numbers: \n${numbersList}`;
+
+        console.log(message);
+
+        const custNum = rows[0].cust_number;
+
+        const apiUrl = `https://dash2.wabot.my/api/send.php?number=${custNum}&type=text&message=${message}&instance_id=65AE763FD8BEE&access_token=10cabf6587ee6a7f6f2d3c8659f56c9a`;
+
+        try {
+            const apiCall = await axios.get(url);
+            console.log(apiCall.data);
+        } catch (error) {
+            console.log(`${error}! Error Making Api Call!`);
+            res.status(500).json({status: 500, message: `Error Making Api Call!`});
+        }
+
+        res.status(200).json({status: 200, message: `Message succesfully sent!`});
+        
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ status: 500, message: 'Internal Server Error' });
+    }
+}
+
 module.exports = {
     getAllCust,
     assignCustomerTask,
-    getAllByCust
+    getAllByCust,
+    getAllTargetNumbers,
+    sendMessage
 }
--- api/targetNumbers/tnRouter.js
+++ api/targetNumbers/tnRouter.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 
 router.get('/get/:custId', tnController.getAllByCust);
 router.post('/assign', tnController.assignCustomerTask);
-router.get('/get', tnController.getAllCust);
+router.get('/get', tnController.getAllCust); 
+router.get('/test', tnController.getAllTargetNumbers);
+router.get('/send/:custId', tnController.sendMessage);
 
 module.exports = router;
