2a4cc07974fd2811929bfd85f71079aa840196d4
Fahad Javed <fahad.js@yahoo.com>
2024-01-27 13:47:03 +0500

    removed

--- api/order/orderRouter.js
+++ api/order/orderRouter.js
@@ -3,7 +3,7 @@ const orderController = require('./orderController');
 const token = require('../../jwt/jwt');
 
 router.post('/:id', token.verifyToken, orderController.create);
-router.get('/res/:restaurant_id', token.verifyToken, orderController.getAllOrders);
+router.get('/res/:restaurant_id', orderController.getAllOrders);
 router.get('/:id', token.verifyToken, orderController.getOrderById);
 
 module.exports = router;
