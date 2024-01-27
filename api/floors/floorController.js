578e7a3dbb10e51e3b0a4d78ac5081d7a875d8de
Fahad Javed <fahad.js@yahoo.com>
2024-01-27 22:29:09 +0500

    updated table_id

--- api/floors/floorController.js
+++ api/floors/floorController.js
@@ -3,7 +3,7 @@ const poolConnection = require('../../config/database');
 const getById = async (req, res) => {
     try {
         let id = req.params.id;
-        let sql = `SELECT * FROM floor_plans JOIN floors ON floor_plans.restaurant_id = floors.restaurant_id LEFT JOIN tables ON floors.floor_id = tables.floor_id LEFT JOIN orders on tables.table_id = orders.table_id WHERE floor_plans.restaurant_id = ?`;
+        let sql = `SELECT *, tables.table_id FROM floor_plans JOIN floors ON floor_plans.restaurant_id = floors.restaurant_id LEFT JOIN tables ON floors.floor_id = tables.floor_id LEFT JOIN orders on tables.table_id = orders.table_id WHERE floor_plans.restaurant_id = ?`;
         let values = id;
         const result = await poolConnection.query(sql, values);
 
