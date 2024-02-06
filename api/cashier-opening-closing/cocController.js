a7a870ff60c008d7f1e10185a537fb21b023c2f1
Fahad Javed <fahad.js@yahoo.com>
2024-02-06 17:36:46 +0500

    added get and post for pos closing

--- api/cashier-opening-closing/cocController.js
+++ api/cashier-opening-closing/cocController.js
@@ -1,9 +1,10 @@
 const poolConnection = require('../../config/database');
+const moment = require('moment-timezone');
 
 const getDenominations = async (req, res) => {
     try {
         // await poolConnection.query('START TRANSACTION');
-        const {restaurant_id} = req.params;
+        const { restaurant_id } = req.params;
 
         const getRestaurant = `
             SELECT
@@ -18,18 +19,17 @@ const getDenominations = async (req, res) => {
                 rest.restaurant_id = ?;
         `;
         const getRestaurantRes = await poolConnection.query(getRestaurant, [restaurant_id]);
-        // const denomination = getRestaurantRes[0];
 
         const formattedOutput = {
             restaurant_id: null,
             name: null,
             denomination_details: []
         };
-    
+
         if (getRestaurantRes.length > 0) {
             formattedOutput.restaurant_id = getRestaurantRes[0].restaurant_id;
             formattedOutput.name = getRestaurantRes[0].name;
-    
+
             formattedOutput.denomination_details = getRestaurantRes.map(row => ({
                 denom_details_id: row.denom_details_id,
                 digit_value: parseFloat(row.digit_value),
@@ -37,14 +37,107 @@ const getDenominations = async (req, res) => {
             }));
         }
 
-        res.json(formattedOutput);
-        
+        res.status(200).json(formattedOutput);
     } catch (error) {
         console.log(`Error! ${error.message}`);
-        res.status(500).json({status: 500, message: error.message});
+        res.status(500).json({ status: 500, message: error.message });
+    }
+}
+
+const posClosing = async (req, res) => {
+    try {
+        await poolConnection.query(`START TRANSACTION`);
+
+        const { restaurant_id } = req.params;
+        const { username, total, items } = req.body;
+
+        const timeZoneQuery = 'SELECT time_zone FROM restaurants WHERE restaurant_id = ?';
+        const timeZoneResult = await poolConnection.query(timeZoneQuery, [restaurant_id]);
+
+        const timeZone = timeZoneResult[0].time_zone;
+        const time = moment.tz(timeZone).format('YYYY-MM-DD HH:mm:ss');
+
+        const posClosing = `INSERT INTO pos_closing (time, restaurant_id, username, total) VALUES (?, ?, ?, ?)`;
+        const posClosingRes = await poolConnection.query(posClosing, [time, restaurant_id, username, total]);
+
+        const posClosingID = posClosingRes.insertId;
+
+        const posClosingDetails = `INSERT INTO pos_closing_details (pos_closing_id, denom_key, denom_value) VALUES (?, ?, ?)`;
+
+        for (const item of items) {
+            const { denomKey, denomValue } = item;
+            await poolConnection.query(posClosingDetails, [posClosingID, denomKey, denomValue]);
+        }
+
+        res.status(201).json({ status: 201, message: `Data Inserted Successfully!` });
+        await poolConnection.query(`COMMIT`);
+    } catch (error) {
+        await poolConnection.query(`ROLLBACK`);
+        console.log(`Error! ${error.message}`);
+        res.status(500).json({ status: 500, message: error.message });
+    }
+}
+
+const getPosClosing = async (req, res) => {
+    try {
+        const { restaurant_id } = req.params;
+
+        const currentDateQuery = `SELECT time_zone, open_time FROM restaurants WHERE restaurant_id = ?`;
+        const currentDateResult = await poolConnection.query(currentDateQuery, [restaurant_id]);
+
+        if (!currentDateResult[0] || currentDateResult[0].time_zone === null) {
+            throw new Error("Time zone not available for the restaurant");
+        }
+
+        const { time_zone, open_time } = currentDateResult[0];
+        const timeZone = time_zone;
+
+        // Combine the current date with the opening time
+        const openingTime = moment.tz(timeZone).startOf('day').format('YYYY-MM-DD') + ' ' + open_time;
+
+        const getPosClosing = `
+            SELECT
+                *
+            FROM
+                pos_closing_details pcd
+            JOIN
+                pos_closing pc ON pcd.pos_closing_id = pc.pos_closing_id
+            WHERE
+                pc.time >= ? AND restaurant_id = ?
+            ORDER BY
+                pc.time;
+        `;
+
+        const getPosClosingRes = await poolConnection.query(getPosClosing, [open_time, restaurant_id]);
+
+        const formattedOutput = {
+            restaurant_id: null,
+            total: null,
+            time: null,
+            pos_closing_details: []
+        };
+
+        if (getPosClosingRes.length > 0) {
+            formattedOutput.restaurant_id = getPosClosingRes[0].restaurant_id;
+            formattedOutput.total = getPosClosingRes[0].total;
+            formattedOutput.time = getPosClosingRes[0].time;
+
+            formattedOutput.pos_closing_details = getPosClosingRes.map(row => ({
+                pos_closing_id: row.pos_closing_id,
+                denom_value: row.denom_value,
+                denom_key: row.denom_key,
+            }));
+        }
+
+        res.status(200).json(formattedOutput);
+    } catch (error) {
+        console.log(`Error! ${error.message}`);
+        res.status(500).json({ status: 500, message: error.message });
     }
 }
 
 module.exports = {
-    getDenominations
+    getDenominations,
+    posClosing,
+    getPosClosing
 }
--- api/cashier-opening-closing/cocRouter.js
+++ api/cashier-opening-closing/cocRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const cocController = require('./cocController');
 
 router.get('/res/:restaurant_id/denom/get', cocController.getDenominations);
+router.get('/res/:restaurant_id/posclosing/get', cocController.getPosClosing);
 
 module.exports = router;
