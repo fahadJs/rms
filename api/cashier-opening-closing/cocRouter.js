a9c881291fda0d5bc740126d886576a589e678c2
Fahad Javed <fahad.js@yahoo.com>
2024-02-06 17:43:32 +0500

    added url for pos closing post

--- api/cashier-opening-closing/cocRouter.js
+++ api/cashier-opening-closing/cocRouter.js
@@ -3,5 +3,6 @@ const cocController = require('./cocController');
 
 router.get('/res/:restaurant_id/denom/get', cocController.getDenominations);
 router.get('/res/:restaurant_id/posclosing/get', cocController.getPosClosing);
+router.post('/res/:restaurant_id/posclosing/create', cocController.posClosing);
 
 module.exports = router;
