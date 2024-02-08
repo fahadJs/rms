b8b5d68b899f596a0f1ba7a0c673ce92c3565102
Fahad Javed <fahad.js@yahoo.com>
2024-02-08 20:19:31 +0500

    updated headers

--- api/cashier-opening-closing/cocRouter.js
+++ api/cashier-opening-closing/cocRouter.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const cocController = require('./cocController');
+const token = require('../../jwt/jwt');
 
-router.get('/res/:restaurant_id/denom/get', cocController.getDenominations);
-router.get('/res/:restaurant_id/posclosing/get', cocController.getPosClosing);
-router.post('/res/:restaurant_id/posclosing/create', cocController.posClosing);
-router.get('/res/:restaurant_id/cashin/get', cocController.getCashIn);
-router.get('/res/:restaurant_id/cashout/get', cocController.getCashOut);
-router.post('/res/:restaurant_id/cashin/create', cocController.cashIn);
-router.post('/res/:restaurant_id/cashout/create', cocController.cashOut);
+router.get('/res/:restaurant_id/denom/get', token.verifyToken, cocController.getDenominations);
+router.get('/res/:restaurant_id/posclosing/get',token.verifyToken, cocController.getPosClosing);
+router.post('/res/:restaurant_id/posclosing/create',token.verifyToken, cocController.posClosing);
+router.get('/res/:restaurant_id/cashin/get',token.verifyToken, cocController.getCashIn);
+router.get('/res/:restaurant_id/cashout/get',token.verifyToken, cocController.getCashOut);
+router.post('/res/:restaurant_id/cashin/create',token.verifyToken, cocController.cashIn);
+router.post('/res/:restaurant_id/cashout/create',token.verifyToken, cocController.cashOut);
 
 module.exports = router;
--- api/targetNumbers/tnRouter.js
+++ api/targetNumbers/tnRouter.js
@@ -1,13 +1,14 @@
 const tnController = require('./tnController');
 const router = require('express').Router();
+const token = require('../../jwt/jwt');
 
-router.get('/get/:custId', tnController.getAllByCust);
-router.post('/assign', tnController.assignCustomerTask);
-router.get('/get', tnController.getAllCust); 
-router.get('/send/:custId', tnController.sendMessage);
-router.post('/updatestatus/:custId', tnController.updateTargetNumbersStatus);
-router.get('/resolve/:custId', tnController.resolveTask);
-router.get('/reassign/:custId', tnController.reAssignCustomerTask);
-router.get('/info', tnController.getAllInfo); 
+router.get('/get/:custId',token.verifyToken, tnController.getAllByCust);
+router.post('/assign',token.verifyToken, tnController.assignCustomerTask);
+router.get('/get',token.verifyToken, tnController.getAllCust); 
+router.get('/send/:custId',token.verifyToken, tnController.sendMessage);
+router.post('/updatestatus/:custId',token.verifyToken, tnController.updateTargetNumbersStatus);
+router.get('/resolve/:custId',token.verifyToken, tnController.resolveTask);
+router.get('/reassign/:custId',token.verifyToken, tnController.reAssignCustomerTask);
+router.get('/info',token.verifyToken, tnController.getAllInfo); 
 
 module.exports = router;
