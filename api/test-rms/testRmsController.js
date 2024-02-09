1a7e3117d06569a46accad302585cecccbbeb2f3
Fahad Javed <fahad.js@yahoo.com>
2024-02-09 18:54:31 +0500

    test post query to cpanel

--- api/test-rms/testRmsController.js
+++ api/test-rms/testRmsController.js
@@ -6,8 +6,11 @@ const test = async (req, res) => {
         const testQuery = `SELECT * FROM restaurants`;
         const output = await poolConnection.query(testQuery);
 
+        const testInsertQuery = `INSERT INTO admins (login_id, login_pass, restaurant_id) VALUES (test, test, 1);`;
+        const insertOutput = await poolConnection.query(testInsertQuery);
+
         console.log(`Connection Successful with the cPanel Database`);
-        res.status(200).json({status: 200, message: `Connection Successful with the cPanel Database`, result: output});
+        res.status(200).json({status: 200, message: `Connection Successful with the cPanel Database`, insertOutput: insertOutput, result: output});
     } catch (error) {
         console.log(error.message);
         res.status(500).json({status: 500, message: error.message});
