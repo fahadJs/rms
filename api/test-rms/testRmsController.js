eed70812900ef4a756f5087fbb772322a388ebbd
Fahad Javed <fahad.js@yahoo.com>
2024-02-26 12:58:46 +0500

    Update testRmsController.js

--- api/test-rms/testRmsController.js
+++ api/test-rms/testRmsController.js
@@ -6,11 +6,11 @@ const test = async (req, res) => {
         const testQuery = `SELECT * FROM restaurants`;
         const output = await poolConnection.query(testQuery);
 
-        const testInsertQuery = `INSERT INTO cash_in (narration, amount, time, restaurant_id) VALUES (?, ?, ?, ?);`;
-        const insertOutput = await poolConnection.query(testInsertQuery, ['FROM AWS', '1000000', '0', 1]);
+        // const testInsertQuery = `INSERT INTO cash_in (narration, amount, time, restaurant_id) VALUES (?, ?, ?, ?);`;
+        // const insertOutput = await poolConnection.query(testInsertQuery, ['FROM AWS', '1000000', '0', 1]);
 
         console.log(`Connection Successful with the cPanel Database`);
-        res.status(200).json({status: 200, message: `Connection Successful with the cPanel Database`, insertOutput: insertOutput, result: output});
+        res.status(200).json({status: 200, message: `Connection Successful with the cPanel Database`, result: output});
     } catch (error) {
         console.log(error.message);
         res.status(500).json({status: 500, message: error.message});
